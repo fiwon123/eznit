c00ab0e5d54757e51e1cfc4ad3cb05b531ba37f8
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-02-23 12:39:23 -0300

    implemented update file

--- cmd/cli/upload.go
+++ cmd/cli/upload.go
@@ -14,7 +14,8 @@ import (
 )
 
 type UploadCmd struct {
-	Args []string `arg:"" optional:"" help:"args for upload"`
+	Update string   `help:"update a file using id"`
+	Args   []string `arg:"" optional:"" help:"args for upload"`
 }
 
 func (cmd *UploadCmd) Run(g *Globals) error {
@@ -28,10 +29,56 @@ func (cmd *UploadCmd) Run(g *Globals) error {
 		return fmt.Errorf("not logged in")
 	}
 
-	err = uploadFile(g.BaseURL+"/v1/files", path, token)
+	if cmd.Update != "" {
+		err = updateFile(fmt.Sprintf(g.BaseURL+"/v1/files/%s", cmd.Update), path, token)
+	} else {
+		err = uploadFile(g.BaseURL+"/v1/files", path, token)
+	}
+
 	if err != nil {
 		return fmt.Errorf("%s", err.Error())
 	}
+
+	return nil
+}
+
+func updateFile(url string, filePath string, token string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile("file", filepath.Base(filePath))
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return err
+	}
+	writer.Close()
+
+	req, err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
 	return nil
 }
