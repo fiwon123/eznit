d436ef9d6e43efeb2ea9b68b9eac2ff0777294f6
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-02-23 19:56:04 -0300

    add log on user repository

--- internal/domain/users/repository_sql.go
+++ internal/domain/users/repository_sql.go
@@ -1,7 +1,7 @@
 package users
 
 import (
-	"fmt"
+	"log/slog"
 
 	"github.com/fiwon123/eznit/pkg/logger"
 	"github.com/jmoiron/sqlx"
@@ -22,10 +22,12 @@ func NewRepository(db *sqlx.DB, logger *logger.Config) *sqlRepository {
 func (r *sqlRepository) GetUsers() []User {
 	var users []User
 
+	r.logger.Debug("GetUsers")
+
 	query := "SELECT id,email,password,created_at FROM users"
 	rows, err := r.db.Query(query)
 	if err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return nil
 	}
 	defer rows.Close()
@@ -34,7 +36,7 @@ func (r *sqlRepository) GetUsers() []User {
 		var user User
 		err := rows.Scan(&user.ID, &user.Email, &user.Password, &user.CreatedAt)
 		if err != nil {
-			fmt.Println(err)
+			r.logger.Error("error: ", slog.Any("error", err))
 			return nil
 		}
 
@@ -45,13 +47,14 @@ func (r *sqlRepository) GetUsers() []User {
 }
 
 func (r *sqlRepository) GetUser(id string) *User {
+	r.logger.Debug("GetUser", slog.String("id", id))
 
 	query := "SELECT id,email,password,created_at,updated_at FROM users WHERE id=$1"
 	row := r.db.QueryRow(query, id)
 
 	var user User
 	if err := row.Scan(&user.ID, &user.Email, &user.Password, &user.CreatedAt); err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return nil
 	}
 
@@ -59,13 +62,14 @@ func (r *sqlRepository) GetUser(id string) *User {
 }
 
 func (r *sqlRepository) GetUserByEmail(email string) *User {
+	r.logger.Debug("GetUserByEmail", slog.String("email", email))
 
 	query := "SELECT id,email,password,created_at,updated_at FROM users WHERE email=$1"
 	row := r.db.QueryRow(query, email)
 
 	var user User
 	if err := row.Scan(&user.ID, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt); err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return nil
 	}
 
@@ -73,17 +77,20 @@ func (r *sqlRepository) GetUserByEmail(email string) *User {
 }
 
 func (r *sqlRepository) UserExists(email string) bool {
+	r.logger.Debug("UserExists", slog.String("email", email))
+
 	var count int
 
 	query := "SELECT COUNT(*) FROM users WHERE email=$1"
 	err := r.db.QueryRow(query, email).Scan(&count)
 
 	if err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return false
 	}
 
 	if count == 0 {
+		r.logger.Debug("user not found")
 		return false
 	}
 
@@ -91,11 +98,13 @@ func (r *sqlRepository) UserExists(email string) bool {
 }
 
 func (r *sqlRepository) CreateUser(user User) bool {
+	r.logger.Debug("CreateUser", slog.Any("user", user))
+
 	exec := "INSERT INTO users (email,password) VALUES ($1, $2)"
 
 	_, err := r.db.Exec(exec, user.Email, user.Password)
 	if err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return false
 	}
 
@@ -103,11 +112,13 @@ func (r *sqlRepository) CreateUser(user User) bool {
 }
 
 func (r *sqlRepository) DeleteUser(user User) bool {
+	r.logger.Debug("DeleteUser", slog.Any("user", user))
+
 	exec := "DELETE FROM users WHERE id=$1"
 
 	_, err := r.db.Exec(exec, user.ID)
 	if err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return false
 	}
 
@@ -115,11 +126,13 @@ func (r *sqlRepository) DeleteUser(user User) bool {
 }
 
 func (r *sqlRepository) UpdateUser(user User) bool {
+	r.logger.Debug("UpdateUser", slog.Any("user", user))
+
 	exec := "UPDATE users SET email=$2, password=$3 WHERE id=$1"
 
 	_, err := r.db.Exec(exec, user.ID, user.Email, user.Password)
 	if err != nil {
-		fmt.Println(err)
+		r.logger.Error("error: ", slog.Any("error", err))
 		return false
 	}
 
