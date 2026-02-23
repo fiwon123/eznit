024d9551870d918c97094b734e14a05fac35e831
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-02-23 20:02:54 -0300

    add logs on user service

--- internal/domain/users/service.go
+++ internal/domain/users/service.go
@@ -1,7 +1,7 @@
 package users
 
 import (
-	"fmt"
+	"log/slog"
 
 	"github.com/fiwon123/eznit/internal/domain/sessions"
 	"github.com/fiwon123/eznit/pkg/logger"
@@ -22,6 +22,7 @@ func NewService(db Repository, session *sessions.Service, logger *logger.Config)
 }
 
 func (s *Service) GetUsers() []DataResponse {
+	s.logger.Debug("GetUsers")
 
 	usersModel := s.db.GetUsers()
 
@@ -36,9 +37,11 @@ func (s *Service) GetUsers() []DataResponse {
 }
 
 func (s *Service) GetUser(id string) (DataResponse, bool) {
+	s.logger.Debug("GetUser", slog.String("id", id))
 
 	user := s.db.GetUser(id)
 	if user == nil {
+		s.logger.Error("User not found")
 		return DataResponse{}, false
 	}
 
@@ -50,14 +53,17 @@ func (s *Service) GetUser(id string) (DataResponse, bool) {
 }
 
 func (s *Service) SignupUser(req SignupRequest) (MsgResponse, bool) {
+	s.logger.Debug("SignupUser", slog.Any("request", req))
 
 	if req.Password != req.ConfirmPassword {
+		s.logger.Error("passwords not match")
 		return MsgResponse{
 			Msg: "passwords not match",
 		}, false
 	}
 
 	if req.Password == "" {
+		s.logger.Error("passwords is empty")
 		return MsgResponse{
 			Msg: "passwords is empty",
 		}, false
@@ -70,20 +76,25 @@ func (s *Service) SignupUser(req SignupRequest) (MsgResponse, bool) {
 }
 
 func (s *Service) LoginUser(req LoginRequest) (LoginResponse, bool) {
+	s.logger.Debug("LoginUser", slog.Any("request", req))
+
 	db := s.db
 
 	user := db.GetUserByEmail(req.Email)
 	if user == nil {
+		s.logger.Error("user not found")
 		return LoginResponse{}, false
 	}
 
 	if !checkPasswordHash(req.Password, user.Password) {
+		s.logger.Error("passwords not match")
 		return LoginResponse{}, false
 	}
 
-	fmt.Println("user logged in")
+	s.logger.Debug("user logged in")
 	token, ok := s.session.CreateToken(user.ID)
 	if !ok {
+		s.logger.Error("create token failed")
 		return LoginResponse{}, false
 	}
 
@@ -93,16 +104,19 @@ func (s *Service) LoginUser(req LoginRequest) (LoginResponse, bool) {
 }
 
 func (s *Service) CreateUser(req CreateRequest) (MsgResponse, bool) {
+	s.logger.Debug("CreateUser", slog.Any("request", req))
 
 	db := s.db
 
 	if db.UserExists(req.Email) {
+		s.logger.Error("user already exists", slog.String("email", req.Email))
 		return MsgResponse{
 			Msg: "user already exists",
 		}, false
 	}
 
 	if req.Password == "" {
+		s.logger.Error("password is empty")
 		return MsgResponse{
 			Msg: "password is empty",
 		}, false
@@ -110,6 +124,7 @@ func (s *Service) CreateUser(req CreateRequest) (MsgResponse, bool) {
 
 	hash, err := hashPassword(req.Password)
 	if err != nil {
+		s.logger.Error("password error: ", slog.Any("error", err))
 		return MsgResponse{
 			Msg: "internal server error",
 		}, false
@@ -119,10 +134,13 @@ func (s *Service) CreateUser(req CreateRequest) (MsgResponse, bool) {
 		Email:    req.Email,
 		Password: hash,
 	}) {
+		s.logger.Error("create user failed")
 		return MsgResponse{
 			Msg: "internal server error",
 		}, false
 	}
+
+	s.logger.Debug("user created!")
 
 	return MsgResponse{
 		Msg: "user created!",
@@ -130,21 +148,26 @@ func (s *Service) CreateUser(req CreateRequest) (MsgResponse, bool) {
 }
 
 func (s *Service) DeleteUser(req DeleteRequest) (MsgResponse, bool) {
+	s.logger.Debug("DeleteUser", slog.Any("request", req))
 
 	db := s.db
 
 	user := db.GetUser(req.Id)
 	if user == nil {
+		s.logger.Error("user not found")
 		return MsgResponse{
 			Msg: "user not exists",
 		}, false
 	}
 
 	if !db.DeleteUser(*user) {
+		s.logger.Error("delete user failed")
 		return MsgResponse{
 			Msg: "internal server error",
 		}, false
 	}
+
+	s.logger.Debug("user deleted!")
 
 	return MsgResponse{
 		Msg: "user deleted!",
@@ -152,17 +175,20 @@ func (s *Service) DeleteUser(req DeleteRequest) (MsgResponse, bool) {
 }
 
 func (s *Service) UpdateUser(req UpdateRequest) (MsgResponse, bool) {
+	s.logger.Debug("UpdateUser", slog.Any("request", req))
 
 	db := s.db
 
 	user := db.GetUser(req.Id)
 	if user == nil {
+		s.logger.Error("user not found")
 		return MsgResponse{
 			Msg: "user not exists",
 		}, false
 	}
 
 	if req.Password == "" {
+		s.logger.Error("password is empty")
 		return MsgResponse{
 			Msg: "password is empty",
 		}, false
@@ -170,6 +196,7 @@ func (s *Service) UpdateUser(req UpdateRequest) (MsgResponse, bool) {
 
 	hash, err := hashPassword(req.Password)
 	if err != nil {
+		s.logger.Error("password error: ", slog.Any("error", err))
 		return MsgResponse{
 			Msg: "internal server error",
 		}, false
@@ -179,10 +206,13 @@ func (s *Service) UpdateUser(req UpdateRequest) (MsgResponse, bool) {
 	user.Password = hash
 
 	if !db.UpdateUser(*user) {
+		s.logger.Error("update user failed")
 		return MsgResponse{
 			Msg: "internal server error",
 		}, false
 	}
+
+	s.logger.Debug("user updated!")
 
 	return MsgResponse{
 		Msg: "user updated!",
