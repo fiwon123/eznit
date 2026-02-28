dce4a406668966ad734a3d163ed41ea8b94f85bf
Your <you@example.com>
2026-02-28 11:09:26 -0300

    fix powershell script add_to_path

--- scripts/add_to_path.ps1
+++ scripts/add_to_path.ps1
@@ -1,13 +1,15 @@
 $currentPath = Get-Location | Select-Object -ExpandProperty Path
 
-$oldPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
+$oldPath = [Environment]::GetEnvironmentVariable("Path", "User")
 
 if ($oldPath -split ';' -contains $currentPath) {
     Write-Host "Path is already present in the System variable." -ForegroundColor Yellow
 } else {
     $newPath = "$oldPath;$currentPath"
 
-    [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+    [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
 
     Write-Host "Successfully added $currentPath to the System PATH." -ForegroundColor Green
 }
+
+Read-Host "Press Enter to exit"
